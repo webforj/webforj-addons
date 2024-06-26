e09b7c37c718440da40f6f4ef2ae953ba8540750
Elyas <elyassalar8@gmail.com>
2024-06-26 18:47:20 +0300

    refactor: suggestion edit component with new requirements

---
+++ webforj-addons-components/webforj-suggestion-edit/src/main/java/com/webforj/addons/components/suggestionedit/events/ChangedEvent.java
@@ -0,0 +1,49 @@
+package com.webforj.addons.components.suggestionedit.events;
+
+import com.google.gson.Gson;
+import com.webforj.addons.components.suggestionedit.Suggestion;
+import com.webforj.addons.components.suggestionedit.SuggestionEdit;
+import com.webforj.component.element.annotation.EventName;
+import com.webforj.component.element.annotation.EventOptions;
+import com.webforj.component.event.ComponentEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Event representing the change in value of the SuggestionEdit field to a suggestion.
+ *
+ * <p>
+ * The {@code ChangedEvent} is emitted when the value of the component changes to a suggestion. This
+ * change can occur either by clicking on a suggestion in the dropdown or by pressing the Enter key
+ * on a suggestion.
+ * </p>
+ *
+ * @since 1.00
+ * @author ElyasSalar
+ */
+@EventName("dwc-changed")
+@EventOptions(data = {@EventOptions.EventData(key = "suggestion", exp = "event.detail")})
+public class ChangedEvent extends ComponentEvent<SuggestionEdit> {
+
+  /**
+   * Creates a new {@code ChangedEvent} with the specified source and input value.
+   *
+   * @param component The component that fired the event.
+   * @param payload The event map.
+   */
+  public ChangedEvent(SuggestionEdit component, Map<String, Object> payload) {
+    super(component, payload);
+  }
+
+  /**
+   * Gets the selected suggestion of the SuggestionEdit field.
+   *
+   * @return The new suggestion.
+   */
+  public Suggestion getSuggestion() {
+    final var gson = new Gson();
+    final var json = gson.toJson(this.getEventMap().get("suggestion"), HashMap.class);
+    return gson.fromJson(json, Suggestion.class);
+  }
+}
---
+++ webforj-addons-components/webforj-suggestion-edit/src/main/java/com/webforj/addons/components/suggestionedit/events/InputEvent.java
@@ -0,0 +1,43 @@
+package com.webforj.addons.components.suggestionedit.events;
+
+import com.webforj.addons.components.suggestionedit.SuggestionEdit;
+import com.webforj.component.element.annotation.EventName;
+import com.webforj.component.element.annotation.EventOptions;
+import com.webforj.component.event.ComponentEvent;
+
+import java.util.Map;
+
+/**
+ * Event representing user input in the SuggestionEdit field.
+ *
+ * <p>
+ * The {@code InputEvent} is emitted whenever the user types in the SuggestionEdit field. This event
+ * is fired on any input change, regardless of whether the input matches a suggestion or not.
+ * </p>
+ *
+ * @since 1.00
+ * @author ElyasSalar
+ */
+@EventName("dwc-input")
+@EventOptions(data = {@EventOptions.EventData(key = "value", exp = "event.detail")})
+public class InputEvent extends ComponentEvent<SuggestionEdit> {
+
+  /**
+   * Creates a new {@code InputEvent} with the specified source and input value.
+   *
+   * @param component The component that fired the event.
+   * @param payload The event map.
+   */
+  public InputEvent(SuggestionEdit component, Map<String, Object> payload) {
+    super(component, payload);
+  }
+
+  /**
+   * Gets the current input value of the SuggestionEdit field.
+   *
+   * @return The current input value.
+   */
+  public String getValue() {
+    return (String) this.getEventMap().get("value");
+  }
+}
