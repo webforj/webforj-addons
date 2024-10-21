3da58092e7a9f05b2b9a99dc134ff05a80ae1316
Elyas <elyassalar8@gmail.com>
2024-10-21 12:04:54 +0300

    feat: setting ignored dimension setter and adding documentation

--- webforj-addons-components/webforj-draw-panel/src/main/java/com/webforj/addons/components/drawpanel/DrawPanel.java
+++ webforj-addons-components/webforj-draw-panel/src/main/java/com/webforj/addons/components/drawpanel/DrawPanel.java
@@ -18,19 +18,94 @@ import com.basis.startup.type.BBjException;
 import com.basis.startup.type.sysgui.BBjColor;
 import com.basis.util.common.BasisNumber;
 
-import com.webforj.App;
-
+/**
+ * Represents a draw panel component that allows users to enter draw on the panel.
+ *
+ * <p>
+ * This class handles setting up the drawing canvas, managing pen properties, opacity, and dimensions, and
+ * interacting with images on the drawing panel. It also allows toggling between scribble mode and clearing
+ * drawings.
+ * </p>
+ *
+ * <h3>Key Features:</h3>
+ * <ul>
+ * <li>Pen customization (color, width)</li>
+ * <li>Adjustable panel dimensions</li>
+ * <li>Support for image export and import</li>
+ * <li>Configurable opacity and scribble mode</li>
+ * </ul>
+ *
+ * <p>This class should be used in conjunction with a {@link Window} to create an interactive drawing area.</p>
+ *
+ * @see Window
+ * @see BBjDrawPanel
+ *
+ * @author MatthewHawkins
+ * @since 24.12
+ */
 public final class DrawPanel extends Window {
 
-	private BBjDrawPanel drawPanel;
-	private Window window;
-	private boolean scribble;
-	private Color penColor;
-	private int penWidth;
-	private int opacity;
-	private double width;
-	private double height;
+  /**
+   * The {@code BBjDrawPanel} object that represents the drawing canvas where graphics can be drawn.
+   * It provides methods to manipulate the drawing surface such as drawing images and setting drawing attributes.
+   */
+  private BBjDrawPanel drawPanelCanvas;
 
+  /**
+   * The {@code Window} instance where the {@code DrawPanel} is displayed. This field is used to manage
+   * the connection between the drawing panel and the parent window.
+   */
+  private Window window;
+
+  /**
+   * A flag that controls whether the drawing panel is in scribble mode or not. When enabled,
+   * scribble mode allows freehand drawing on the panel.
+   */
+  private boolean scribble;
+
+  /**
+   * The {@code Color} object that defines the pen color used for drawing on the panel.
+   * It defines the color for lines, arrows, and outlines.
+   */
+  private Color penColor;
+
+  /**
+   * The width of the pen used for drawing in pixels.
+   */
+  private int penWidth;
+
+  /**
+   * The opacity of the drawing panel. The value can be {@code 0}, {@code 1}, and {@code 2}.
+   *
+   * <ul>
+   *   <li>{@code 0} - Transparent and not scaled</li>
+   *   <li>{@code 1} - Opaque and not scaled</li>
+   *   <li>{@code 2} - Opaque and scaled</li>
+   * </ul>
+   */
+  private int opacity;
+
+  /**
+   * The width of the drawing panel in pixels. This determines the horizontal size of the panel.
+   */
+  private double width;
+
+  /**
+   * The height of the drawing panel in pixels. This determines the vertical size of the panel.
+   */
+  private double height;
+
+  /**
+   * Default constructor for {@code DrawPanel}.
+   * Initializes the drawing panel with default settings, including:
+   * <ul>
+   * <li>Scribble mode enabled</li>
+   * <li>Pen color set to black</li>
+   * <li>Pen width set to 1 pixel</li>
+   * <li>Opacity set to 1 (fully opaque)</li>
+   * <li>Panel dimensions initialized to 1x1</li>
+   * </ul>
+   */
 	public DrawPanel() {
 		scribble = true;
 		penColor = new Color(0, 0, 0);
@@ -40,6 +115,12 @@ public final class DrawPanel extends Window {
 		height = 1;
 	}
 
+  /**
+   * Constructs a {@code DrawPanel} with specified dimensions.
+   *
+   * @param startWidth  The initial width of the drawing panel.
+   * @param startHeight The initial height of the drawing panel.
+   */
 	public DrawPanel(double startWidth, double startHeight) {
 		this();
 		width = startWidth;
@@ -53,7 +134,7 @@ public final class DrawPanel extends Window {
 		try {
 			BBjWindow w = WindowAccessor.getDefault().getBBjWindow(window);
 			w.setSize(new BasisNumber(width), new BasisNumber(height));
-			this.drawPanel = w.getDrawPanel();
+			this.drawPanelCanvas = w.getDrawPanel();
 			setScribble(scribble);
 			setOpaque(opacity);
 			setPenColor(penColor);
@@ -63,10 +144,16 @@ public final class DrawPanel extends Window {
 		}
 	}
 
+  /**
+   * Returns the {@code BBjImage} of the current drawing on the panel. This is the underlying image format
+   * used by the BBjDrawPanel.
+   *
+   * @return The {@code BBjImage} of the drawing panel, or {@code null} if no image is available.
+   */
 	public BBjImage getDrawPanelBBjImage() {
-		if (drawPanel != null) {
+		if (drawPanelCanvas != null) {
 			try {
-				return drawPanel.getDrawPanelImage();
+				return drawPanelCanvas.getDrawPanelImage();
 			} catch (Exception e) {
 				throw new WebforjRuntimeException(e);
 			}
@@ -74,11 +161,16 @@ public final class DrawPanel extends Window {
 		return null;
 	}
 
+  /**
+   * Returns a {@code BufferedImage} of the current drawing on the panel.
+   *
+   * @return The {@code BufferedImage} of the drawing panel, or {@code null} if no image is available.
+   */
 	public BufferedImage getDrawPanelImage() {
-		if (drawPanel != null) {
+		if (drawPanelCanvas != null) {
 			try {
 				ByteArrayInputStream byteStream = new ByteArrayInputStream(
-						drawPanel.getDrawPanelImage().getBytes("png"));
+						drawPanelCanvas.getDrawPanelImage().getBytes("png"));
 				return ImageIO.read(byteStream);
 			} catch (BBjException | IOException e) {
 				throw new WebforjRuntimeException(e);
@@ -87,10 +179,15 @@ public final class DrawPanel extends Window {
 		return null;
 	}
 
+  /**
+   * Sets the provided {@code BBjImage} onto the drawing panel canvas.
+   *
+   * @param image The {@code BBjImage} to be drawn on the panel.
+   */
 	public void setDrawPanelImage(BBjImage image) {
-		if (drawPanel != null) {
+		if (drawPanelCanvas != null) {
 			try {
-				drawPanel.drawImage(image, new BasisNumber(0), new BasisNumber(0),
+				drawPanelCanvas.drawImage(image, new BasisNumber(0), new BasisNumber(0),
 						new BasisNumber(width), new BasisNumber(height));
 			} catch (Exception e) {
 				throw new WebforjRuntimeException(e);
@@ -98,12 +195,20 @@ public final class DrawPanel extends Window {
 		}
 	}
 
+  /**
+   * Sets the dimensions of the drawing panel.
+   *
+   * @param width  The new width for the drawing panel.
+   * @param height The new height for the drawing panel.
+   * @return The current {@code DrawPanel} instance for chaining calls.
+   */
 	public DrawPanel setDimensions(double width, double height) {
 		this.width = width;
 		this.height = height;
-		if (drawPanel != null) {
+		if (drawPanelCanvas != null) {
 			try {
 				BBjWindow w = WindowAccessor.getDefault().getBBjWindow(window);
+        w.setSize(new BasisNumber(width), new BasisNumber(height));
 			} catch (Exception e) {
 				throw new WebforjRuntimeException(e);
 			}
@@ -111,11 +216,26 @@ public final class DrawPanel extends Window {
 		return this;
 	}
 
+  /**
+   * Sets the opacity of the drawing panel.
+   *
+   * <p>
+   * Valid values are:
+   * <ul>
+   *   <li>{@code 0} - Transparent and not scaled</li>
+   *   <li>{@code 1} - Opaque and not scaled</li>
+   *   <li>{@code 2} - Opaque and scaled</li>
+   * </ul>
+   * </p>
+   *
+   * @param opacity The opacity value can be {@code 0}, {@code 1}, or {@code 2}.
+   * @return The current {@code DrawPanel} instance for chaining calls.
+   */
 	public DrawPanel setOpaque(int opacity) {
 		this.opacity = opacity;
-		if (drawPanel != null) {
+		if (drawPanelCanvas != null) {
 			try {
-				drawPanel.setOpaque(opacity);
+				drawPanelCanvas.setOpaque(opacity);
 			} catch (Exception e) {
 				throw new WebforjRuntimeException(e);
 			}
@@ -123,11 +243,17 @@ public final class DrawPanel extends Window {
 		return this;
 	}
 
+  /**
+   * Sets the pen color for the drawing panel.
+   *
+   * @param penColor The {@code Color} to be used for the pen.
+   * @return The current {@code DrawPanel} instance for chaining calls.
+   */
 	public DrawPanel setPenColor(Color penColor) {
 		this.penColor = penColor;
-		if (drawPanel != null) {
+		if (drawPanelCanvas != null) {
 			try {
-				drawPanel.setPenColor(new BBjColor(penColor));
+				drawPanelCanvas.setPenColor(new BBjColor(penColor));
 			} catch (Exception e) {
 				throw new WebforjRuntimeException(e);
 			}
@@ -135,11 +261,17 @@ public final class DrawPanel extends Window {
 		return this;
 	}
 
+  /**
+   * Sets the pen width for drawing.
+   *
+   * @param penWidth The width of the pen in pixels.
+   * @return The current {@code DrawPanel} instance for chaining calls.
+   */
 	public DrawPanel setPenWidth(int penWidth) {
 		this.penWidth = penWidth;
-		if (drawPanel != null) {
+		if (drawPanelCanvas != null) {
 			try {
-				drawPanel.setPenWidth(new BasisNumber(penWidth));
+				drawPanelCanvas.setPenWidth(new BasisNumber(penWidth));
 			} catch (Exception e) {
 				throw new WebforjRuntimeException(e);
 			}
@@ -147,10 +279,15 @@ public final class DrawPanel extends Window {
 		return this;
 	}
 
+  /**
+   * Clears the current drawing from the panel.
+   *
+   * @return The current {@code DrawPanel} instance for chaining calls.
+   */
 	public DrawPanel clearDrawing() {
-		if (drawPanel != null) {
+		if (drawPanelCanvas != null) {
 			try {
-				drawPanel.clearDrawing();
+				drawPanelCanvas.clearDrawing();
 			} catch (Exception e) {
 				throw new WebforjRuntimeException(e);
 			}
@@ -158,10 +295,16 @@ public final class DrawPanel extends Window {
 		return this;
 	}
 
+  /**
+   * Toggles the scribble mode on the drawing panel. Scribble mode allows freehand drawing.
+   *
+   * @param scribble {@code true} to enable scribble mode, {@code false} to disable it.
+   * @return The current {@code DrawPanel} instance for chaining calls.
+   */
 	public DrawPanel setScribble(boolean scribble) {
-		if (drawPanel != null) {
+		if (drawPanelCanvas != null) {
 			try {
-				drawPanel.setScribble(scribble);
+				drawPanelCanvas.setScribble(scribble);
 				return this;
 			} catch (BBjException e) {
 				throw new WebforjRuntimeException(e);
