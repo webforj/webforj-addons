b828eec7570d11ec5f55fb738bbce1fba403e827
Elyas <elyassalar8@gmail.com>
2024-11-09 13:16:58 +0300

    feat: adding focus and blur events and utility methods to call on client component

---
+++ webforj-addons-components/webforj-multi-select-combo/src/main/java/com/webforj/addons/components/multiselectcombo/events/FocusedEvent.java
@@ -0,0 +1,27 @@
+package com.webforj.addons.components.multiselectcombo.events;
+
+import com.webforj.addons.components.multiselectcombo.MultiSelectCombo;
+import com.webforj.component.element.annotation.EventName;
+import com.webforj.component.event.ComponentEvent;
+
+import java.util.Map;
+
+/**
+ * Event fired when multi-select combo input focused
+ *
+ * @author @ElyasSalar
+ * @since 24.20
+ */
+@EventName("dwc-focused")
+public class FocusedEvent extends ComponentEvent<MultiSelectCombo> {
+
+	/**
+	 * Creates a new event {@code dwc-focused} event.
+	 *
+	 * @param component the component that fired the event
+	 * @param payload the event map
+	 */
+	public FocusedEvent(MultiSelectCombo component, Map<String, Object> payload) {
+		super(component, payload);
+	}
+}
