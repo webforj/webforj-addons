1305ac443b17cfa8ff39baa8074102c8ab385367
Elyas <83926888+ElyasSalar@users.noreply.github.com>
2024-12-11 13:12:17 +0100

    deps: update the DWC version and align with the new changes (#76)
    * deps: update the DWC version and align with the new changes
    
    * refactor: format the new code changes

---
+++ webforj-addons-components/webforj-suggestion-edit/src/main/java/com/webforj/addons/components/suggestionedit/events/OpenedEvent.java
@@ -0,0 +1,27 @@
+package com.webforj.addons.components.suggestionedit.events;
+
+import com.webforj.addons.components.suggestionedit.SuggestionEdit;
+import com.webforj.component.element.annotation.EventName;
+import com.webforj.component.event.ComponentEvent;
+
+import java.util.Map;
+
+/**
+ * Event fired when the suggestion dropdown is opened.
+ *
+ * @author @ElyasSalar
+ * @since 24.12
+ */
+@EventName("dwc-opened")
+public class OpenedEvent extends ComponentEvent<SuggestionEdit> {
+
+	/**
+	 * Creates a new {@code dwc-opened} event.
+	 *
+	 * @param component the component that fired the event
+	 * @param payload the event map
+	 */
+	public OpenedEvent(SuggestionEdit component, Map<String, Object> payload) {
+		super(component, payload);
+	}
+}
