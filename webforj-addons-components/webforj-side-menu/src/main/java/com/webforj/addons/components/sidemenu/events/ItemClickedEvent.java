b0674d9ce5962cdb2718ff14ec6fba204cc5e3c6
Elyas <83926888+ElyasSalar@users.noreply.github.com>
2024-12-13 12:18:04 +0100

    feat: upgrade the dwc client component and add item-click event for SideMenu (#80)

---
+++ webforj-addons-components/webforj-side-menu/src/main/java/com/webforj/addons/components/sidemenu/events/ItemClickedEvent.java
@@ -0,0 +1,39 @@
+package com.webforj.addons.components.sidemenu.events;
+
+import com.webforj.addons.components.sidemenu.Item;
+import com.webforj.addons.components.sidemenu.SideMenu;
+import com.webforj.component.element.annotation.EventName;
+import com.webforj.component.element.annotation.EventOptions;
+import com.webforj.component.event.ComponentEvent;
+
+import java.util.Map;
+
+/**
+ * Event fired when an item is clicked.
+ *
+ * @since 24.20
+ * @author @ElyasSalar
+ */
+@EventName("dwc-item-clicked")
+@EventOptions(data = {@EventOptions.EventData(key = "item", exp = "event.detail")})
+public class ItemClickedEvent extends ComponentEvent<SideMenu> {
+
+	/**
+	 * Creates a new event {@code dwc-item-clicked} event.
+	 *
+	 * @param component the component that fired the event
+	 * @param payload the event map
+	 */
+	public ItemClickedEvent(SideMenu component, Map<String, Object> payload) {
+		super(component, payload);
+	}
+
+	/**
+	 * Gets the clicked item from the side menu.
+	 *
+	 * @return The clicked item.
+	 */
+	public Item getItem() {
+		return (Item) this.getEventMap().get("item");
+	}
+}
