e0bfb2c64d66f1c8d70b7746b225884266b4706b
Elyas <83926888+ElyasSalar@users.noreply.github.com>
2025-01-29 15:13:17 +0100

    deps: update the DWC client version (#87)

--- webforj-addons-components/webforj-components-common/src/main/java/com/webforj/addons/constant/GlobalConstants.java
+++ webforj-addons-components/webforj-components-common/src/main/java/com/webforj/addons/constant/GlobalConstants.java
@@ -11,7 +11,7 @@ public class GlobalConstants {
 	/**
 	 * The version of the DWC client components CDN.
 	 */
-	public static final String DWC_CDN_VERSION = "1.1.2-0";
+	public static final String DWC_CDN_VERSION = "1.2.0";
 
 	/**
 	 * The full URL of the DWC CDN, including the version number. This constant
