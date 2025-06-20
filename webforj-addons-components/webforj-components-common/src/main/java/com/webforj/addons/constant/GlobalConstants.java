5c71b6c4669e8ede960ea7a7aa18cce56d302634
Elyas <83926888+ElyasSalar@users.noreply.github.com>
2025-06-20 19:17:42 +0300

    refactor: externalize DWC CDN version configuration for CI/CD management (#133)

--- webforj-addons-components/webforj-components-common/src/main/java/com/webforj/addons/constant/GlobalConstants.java
+++ webforj-addons-components/webforj-components-common/src/main/java/com/webforj/addons/constant/GlobalConstants.java
@@ -5,13 +5,10 @@ public class GlobalConstants {
 
   private GlobalConstants() {}
 
-  /** The version of the DWC client components CDN. */
-  public static final String DWC_CDN_VERSION = "1.2.2-0";
-
   /**
    * The full URL of the DWC CDN, including the version number. This constant combines the base URL
-   * with the {@link GlobalConstants#DWC_CDN_VERSION} to form the complete CDN URL.
+   * with the version from the property in the POM file to form the complete CDN URL.
    */
   public static final String DWC_CDN =
-      "https://d3hx2iico687v8.cloudfront.net/" + DWC_CDN_VERSION + "/dwc-addons.esm.js";
+      "https://d3hx2iico687v8.cloudfront.net/${dwc.cdn.version}/dwc-addons.esm.js";
 }
